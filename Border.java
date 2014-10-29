58ea4953ca8b77ee6d1efa9ba436c5f451e6ba83
Huayi <huayiguo@users.noreply.github.com>
2014-10-29 12:07:13 -0400

    Create Border.java

---
+++ Border.java
@@ -0,0 +1,21 @@
+package org;
+
+
+
+public class Border {
+	public 
+		String name;
+		String direction;
+	public void Setname(String Name){
+		this.name = Name;
+	}
+	public void Setdirection(String Ndirection){
+		this.direction = Ndirection;
+	}
+	public String Getname(){
+		return this.name;
+	}
+	public String Getdirection(){
+		return this.direction;
+	}
+}
